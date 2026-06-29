40d1bd74b0507eadcb49095550835a55c211ed01
黃文典 <danielhwang@huangwendiandeMacBook-Pro.local>
2026-06-29 14:27:52 +0800

    [ADD] implemented alu_control.v

---
+++ alu_control.v
@@ -0,0 +1,33 @@
+`include "define.v"
+
+module alu_control(
+    input wire [6:0] opcode,
+    input wire [6:0] funct7,
+    input wire [2:0] funct3,
+
+    output reg [3:0] alu_ctrl
+);
+
+always @(*) begin
+    alu_ctrl = `ALU_ADD;
+
+    case (opcode)
+        `OPCODE_R : begin
+            case ({funct7, funct3})
+                {7'b0000000, 3'b000}: alu_ctrl = `ALU_ADD;
+                {7'b0100000, 3'b000}: alu_ctrl = `ALU_SUB;
+                default             : alu_ctrl = `ALU_ADD;
+                
+            endcase
+        end
+        default: begin
+        end
+
+    endcase
+
+
+end
+
+
+
+endmodule
--- define.v
+++ define.v
@@ -8,7 +8,19 @@
 `define INST_WIDTH 32
 
 
+`define OPCODE_R        7'b0110011
+`define OPCODE_I        7'b0010011
+`define OPCODE_LOAD     7'b0000011
+`define OPCODE_STORE    7'b0100011
+`define OPCODE_BRANCH   7'b1100011
+`define OPCODE_JAL      7'b1101111
+`define OPCODE_JALR     7'b1100111
+`define OPCODE_LUI      7'b0110111
+`define OPCODE_AUIPC    7'b0010111
 
+
+`define ALU_ADD 4'b0000
+`define ALU_SUB 4'b0001
 
 
 `endif
