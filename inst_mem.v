20190aaad4ffeb7a76b41a70bf99e42b7c3af1c2
黃文典 <danielhwang@huangwendiandeMacBook-Pro.local>
2026-06-29 10:43:17 +0800

    [FIX] fixed inst_mem.v

--- inst_mem.v
+++ inst_mem.v
@@ -1,25 +1,13 @@
-`define "define.v"
-
+`include "define.v"
 // input addr, output instruction
-
 module inst_mem(
-    input wire                      clk,
-    input wire                      rst,
-    input wire  [`ADDR_WIDTH-1:0]   addr,
-
-    output reg  [`INST_WIDTH-1:0]   inst
+    input  wire [`ADDR_WIDTH-1:0] addr,
+    output wire [`INST_WIDTH-1:0] inst
 );
-    //TODO: add inst mem size in "define.v"
-    reg [`INST_WIDTH-1] mem [0:255];
+    //TODO: add inst mem size in define.v
+    reg [`INST_WIDTH-1:0] mem [0:255];
 
-    always @(posedge clk) begin
-        if(rst) begin
-            inst <= 32'b0;
-        end
-        else begin
-            inst <= mem[addr >> 2];
-        end
-    end
+    assign inst = mem[addr[9:2]]; // byte address to word address
 
 
 endmodule
