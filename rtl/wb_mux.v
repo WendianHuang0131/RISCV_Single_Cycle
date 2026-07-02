ea60b481b36095776861e9abfed8a1d5dff7f525
黃文典 <danielhwang@huangwendiandeMacBook-Pro.local>
2026-07-02 21:40:55 +0800

    [UPDATE] Udated write back module for j-type instrution

--- rtl/wb_mux.v
+++ rtl/wb_mux.v
@@ -1,20 +1,20 @@
 `include "define.v"
 
 module wb_mux(
-    input  wire [1:0]               wb_sel,
-    input  wire [`DATA_WIDTH-1:0]   alu_result,
-    input  wire [`DATA_WIDTH-1:0]   mem_read_data,
-    input  wire [`DATA_WIDTH-1:0]   pc_plus_4,
+    input  wire [1:0]              wb_sel,
+    input  wire [`DATA_WIDTH-1:0]  alu_result,
+    input  wire [`DATA_WIDTH-1:0]  mem_read_data,
+    input  wire [`DATA_WIDTH-1:0]  pc_plus4,
 
-    output reg  [`DATA_WIDTH-1:0]   wb_data
+    output reg  [`DATA_WIDTH-1:0]  wb_data
 );
 
 always @(*) begin
     case (wb_sel)
-        `WB_ALU:  wb_data = alu_result;
-        `WB_MEM:  wb_data = mem_read_data;
-        `WB_PC4:  wb_data = pc_plus_4;
-        default:  wb_data = {`DATA_WIDTH{1'b0}};
+        `WB_ALU: wb_data = alu_result;
+        `WB_MEM: wb_data = mem_read_data;
+        `WB_PC4: wb_data = pc_plus4;
+        default: wb_data = {`DATA_WIDTH{1'b0}};
     endcase
 end
 
